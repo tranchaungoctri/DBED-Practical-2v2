6c4d9e979bcb0ad50208f1cbb8bfba620cbbe3ef
tranchaungoctri <tranchaungoctri@gmail.com>
2024-08-15 07:17:07 +0000

    git commit git push

---
+++ DBEDAssign2.py
@@ -0,0 +1,71 @@
+import mysql.connector
+import math
+
+# DBED Assignment 2
+# Student ID: a1904209
+# Name: Chau Ngoc Tri Tran
+
+class DBEDAssign2():
+    def __init__(self,name):
+        self.name=name
+
+    def disp(self):
+        print(self.name)
+
+    def setUp(self):
+        """Set up the DB connection to the 'postal' database"""
+        self.connection = mysql.connector.connect(database='postal')
+        self.cursor = self.connection.cursor(buffered=True)
+
+    def syncDB(self):
+        """We need to commit after insertion to ensure that the changes stick.
+        syncDB is a wrapper to the connection commit that takes no parameters and
+        returns no result."""
+        self.connection.commit()
+
+    def tearDown(self):
+        """tearDown destroys the cursor and the connection to clean up."""
+        self.cursor.close()
+        self.connection.close()
+
+    def show_all(self):
+        """Select all rows in the database's pcode table and return it as a list to the user"""
+
+
+    def select_by_pcode(self,pcode):
+        """Perform a SELECT * query using the pcode parameter for postcode. Returns the query
+        result as a list object."""
+
+
+    def insert_data(self,pcode,locality,state):
+        """Insert data into the database"""
+
+
+    def readData(self,fname):
+        """Read in the data from the CSV datafile called fname and put it into the database
+        Takes a single string parameter and does not return any values.
+        IMPORTANT: you must call syncDB before exiting or your changes won't stick!"""
+        with open('./'+fname,"r") as csv:
+            # Skip the header
+            csv.readline()
+
+            # Your code here to insert the data
+
+            csv.close()
+            #Commit
+            self.syncDB()
+
+    def entropyCalc(self):
+        """Analyse the postcode data to determine the entropy of the fourth column
+        Takes no parameters and returns a single floating point number that is the
+        total entropy of the fourth column.
+        """
+
+        # Scan the database for the counts
+
+        # Calculate the frequencies and total entropy
+
+        # Return the total entropy
+
+
+
